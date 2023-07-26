0f1882f07d4d856f1b3cf60db823e514e294781b
Harrison Carroll <harrison_2000@live.co.uk>
2023-07-26 11:18:51 +0100

    - Organse files into folders - Set ContentModel as environment object - Rename content view

---
+++ Shared/ViewModels/ContentModel.swift
@@ -0,0 +1,21 @@
+//
+//  ContentModel.swift
+//  LearningApp (iOS)
+//
+//  Created by Harrison Carroll on 26/07/2023.
+//
+
+import Foundation
+
+struct ContentModel: ObservableObject {
+    
+    var model:
+    
+    
+    
+    
+    
+    
+    
+    
+}
