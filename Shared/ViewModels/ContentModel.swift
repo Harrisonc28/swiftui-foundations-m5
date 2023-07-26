52e9b1b844d7e7f078165e3b418ee896bfd49dea
Harrison Carroll <harrison_2000@live.co.uk>
2023-07-26 12:07:18 +0100

    - assets - Add data files - Parse style data

--- Shared/ViewModels/ContentModel.swift
+++ Shared/ViewModels/ContentModel.swift
@@ -7,11 +7,53 @@
 
 import Foundation
 
-struct ContentModel: ObservableObject {
+class ContentModel: ObservableObject {
     
-    var model:
+    @Published var modules = [Module]()
+    @Published var styleData: Data?
     
+    init() {
+        
+        getLocalData()
+        
+        
+    }
     
+    func getLocalData() {
+        
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        do {
+        let jsonData = try Data(contentsOf: jsonUrl!)
+       let jsonDecoder = JSONDecoder()
+           
+       let dataService = try jsonDecoder.decode([Module].self, from: jsonData)
+       
+            self.modules = dataService
+    
+        }
+        catch {
+            
+            print(error)
+        }
+        
+        
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        do {
+            
+            
+            let styleData = try Data(contentsOf: styleUrl!)
+            
+            self.styleData = styleData
+            
+        } catch {
+            
+            
+           print(error)
+            
+            
+        }
+        
+    }
     
     
     
